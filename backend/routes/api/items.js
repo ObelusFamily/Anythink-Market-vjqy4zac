48d80ed8efd536f0738a097c1618a08badbb0daa
Rene Duivenvoorden <rduivenvoorden@bouwatch.nl>
2023-01-27 11:40:42 +0100

    Fix item image placeholder on the api side

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -87,6 +87,7 @@ router.get("/", auth.optional, function(req, res, next) {
           items: await Promise.all(
             items.map(async function(item) {
               item.seller = await User.findById(item.seller);
+              item.image = item.image.length !== 0 ? item.image : '/placeholder.png'
               return item.toJSONFor(user);
             })
           ),
